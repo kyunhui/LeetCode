0cfcb7cd13a539e2564c6e3439a55968c60c3258
YunHui Kim <106079352+kyunhui@users.noreply.github.com>
2025-02-05 23:24:16 +0900

    Time: 521 ms (91.04%), Space: 21.5 MB (91.13%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,27 +1,25 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        num_node = len(points)
-        q = [(0, 0)]
-        visit = set()
-        
-        num_edges = 0
-        total_cost = 0
-        
-        while num_edges < num_node:
-            cost, current = heapq.heappop(q)
-            
-            if current in visit:
-                continue 
-                
-            num_edges += 1
-            visit.add(current)
-            total_cost += cost 
-            
-            x1, y1 = points[current][0], points[current][1]
-            
-            #현재 노드에서 모든 cost 계산
-            for node, (x2, y2) in enumerate(points):
-                dist = abs(x1-x2) + abs(y1-y2)
-                heapq.heappush(q, (dist, node))
-                
-        return total_cost
+        n = len(points)
+        min_cost = 0
+        visited = [False] * n
+        pq = [(0, 0)]  # (cost, vertex)
+        cache = {0: 0}
+
+        while pq:
+            cost, u = heapq.heappop(pq)
+
+            if visited[u]:
+                continue
+
+            visited[u] = True
+            min_cost += cost
+
+            for v in range(n):
+                if not visited[v]:
+                    dist = abs(points[u][0] - points[v][0]) + abs(points[u][1] - points[v][1])
+                    if dist < cache.get(v, float('inf')):
+                        cache[v] = dist
+                        heapq.heappush(pq, (dist, v))
+
+        return min_cost        
